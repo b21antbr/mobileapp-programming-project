6111a2004eb67376d4b146c58025ec1a3e439772
b21antbr <b21antbr@student.his.se>
2022-05-24 13:39:59 +0200

    minor fixes in colors and activity, ready for presentation

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -14,7 +14,7 @@ public class SecondActivity extends AppCompatActivity {
         secondTextView = findViewById(R.id.secondTextView);
         secondTextView.setText("En app för att titta på fina öar i grekiska cycladerna. " +
                 "Från unga till gamla, leta semestermål eller se lite information om öarna." +
-                "\n\nMöjligheter för att skala upp applikationen, exempelvis med med omfattande information" +
+                "\n\nMöjligheter för att skala upp applikationen, exempelvis med mer omfattande information" +
                 " samt att utöka till alla grekiska ögrupper.");
 
     }
